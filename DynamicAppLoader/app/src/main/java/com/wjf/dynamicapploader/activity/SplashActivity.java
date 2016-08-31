8d4f63820482bffc73ad1579021c51645d9ac9dc
weijianfeng <676654309@qq.com>
2016-08-31 15:44:20 +0800

    add Easter Egg for SplashActivity.

---
+++ DynamicAppLoader/app/src/main/java/com/wjf/dynamicapploader/activity/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.wjf.dynamicapploader.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.wjf.dynamicapploader.R;
+
+import me.wangyuwei.particleview.ParticleView;
+
+/**
+ * description
+ *
+ * @author weijianfeng @Hangzhou Youzan Technology Co.Ltd
+ * @date 16/8/31
+ */
+public class SplashActivity extends Activity{
+
+    private ParticleView mParticleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mParticleView = (ParticleView)findViewById(R.id.pv);
+
+        mParticleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        mParticleView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mParticleView.startAnim();
+            }
+        }, 200);
+    }
+}
